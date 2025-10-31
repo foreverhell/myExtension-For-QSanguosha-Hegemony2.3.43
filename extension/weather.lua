e4f485203a152530b2537a9a2934a64b846aac16
foreverhell <13949325+yiyangzhe@user.noreply.gitee.com>
2025-10-31 05:23:44 +0000

    增加冰雹天气
    Signed-off-by: foreverhell <13949325+yiyangzhe@user.noreply.gitee.com>

--- extension/weather.lua
+++ extension/weather.lua
@@ -42,6 +42,26 @@ Rainy = sgs.CreateTreasure{
     end  
 }  
 
+Snowy = sgs.CreateTreasure{  
+    name = "Snowy",  
+    class_name = "Snowy",
+    suit = sgs.Card_Club,  
+    number = 13,  
+    on_install = function(self, player)  
+        local room = player:getRoom()  
+        room:setTag("weather", sgs.QVariant("Snowy")) 
+    end,  
+      
+    on_uninstall = function(self, player)  
+        local room = player:getRoom()  
+        --永久天气太强
+        if room:getTag("weather"):toString() == "Snowy" then --当前是雪天，移除后，天气为空
+            room:setTag("weather", sgs.QVariant("")) 
+        end
+        --当前不是雪天，说明已经被覆盖，移除后，天气不变
+    end  
+}  
+
 --晴天，火属性伤害+1
 SunnyFireDamage = sgs.CreateTriggerSkill{  
     name = "SunnyFireDamage",  
@@ -208,8 +228,56 @@ RainyDistance = sgs.CreateDistanceSkill{
 	end
 }
 
+--冰雹：每轮随机对一名角色造成一点伤害/流失一点体力；伤害视为体力流失；体力流失+1
+SnowyEffect = sgs.CreateTriggerSkill{  
+    name = "SnowyEffect",  
+    events = {sgs.EventPhaseStart, sgs.Damage, sgs.PreHpLost},  
+    global = true,      
+    can_trigger = function(self, event, room, player, data)  
+        if room:getTag("weather"):toString() ~= "Snowy" then return "" end
+        if event == sgs.EventPhaseStart and player:getPhase() == sgs.Player_Start then
+            if room:getCurrent() == room:getAlivePlayers():first() then
+                return self:objectName()
+            end
+        elseif evennt == sgs.Damage or event == sgs.PreHpLost then
+            return self:objectName() 
+        end
+        return ""
+    end,  
+      
+    on_cost = function(self, event, room, player, data)  
+        return true
+    end,  
+      
+    on_effect = function(self, event, room, player, data)  
+        if event == sgs.EventPhaseStart and player:getPhase() == sgs.Player_Start then
+            --随机选择一名存活角色
+            local num = room:getAlivePlayers():length()
+            local index = math.random(num)
+            local target = room:getAlivePlayers():at(index)
+            --造成伤害
+            local damage = sgs.DamageStruct()  
+            damage.from = nil  
+            damage.to = target  
+            damage.damage = 1  
+            damage.reason = "Snowy"  
+            room:damage(damage) 
+        elseif event == sgs.Damage then
+            local damage = data:toDamage()
+            local target = damage.to
+            room:loseHp(target, damage.damage)
+            return true
+        elseif event == sgs.PreHpLost then --会不会反复触发？
+            local num = data:toInt()
+            data:setValue(num+1)
+        end
+        return false        
+    end  
+}
+
 Sunny:setParent(extension)
 Rainy:setParent(extension)
+Snowy:setParent(extension)
 if not sgs.Sanguosha:getSkill("SunnyFireDamage") then
     skills:append(SunnyFireDamage)
 end
@@ -238,6 +306,10 @@ end
 if not sgs.Sanguosha:getSkill("RainyDistance") then
     skills:append(RainyDistance)
 end
+
+if not sgs.Sanguosha:getSkill("SnowyEffect") then
+    skills:append(SnowyEffect)
+end
 sgs.Sanguosha:addSkills(skills)
 sgs.LoadTranslationTable{
     ["weather"] = "天气包",
@@ -245,6 +317,8 @@ sgs.LoadTranslationTable{
     [":Sunny"] = "装备时，将天气改为晴天，火属性伤害+1，雷属性伤害-1，摸牌+1，手牌上限+1，攻击范围+1，治疗量+1；失去时，若天气为晴天，则改为无天气",
     ["Rainy"] = "雨天",
     [":Rainy"] = "装备时，将天气改为雨天，火属性伤害为0，雷属性伤害+1，所有角色到其他角色的距离+1；失去时，若天气为雨天，则改为无天气",
+    ["Snowy"] = "冰雹",
+    [":Snowy"] = "每轮随机对一名角色造成一点伤害；所有伤害均视为体力流失；所有体力流失量+1",
 --晴天，火属性伤害+1，雷属性伤害-1，摸牌+1，手牌上限+1，攻击范围+1，治疗量+1
 --雨天，火属性伤害为0，雷属性伤害+1，所有角色到其他角色的距离+1，反转判定结果
 --可以考虑添加：
