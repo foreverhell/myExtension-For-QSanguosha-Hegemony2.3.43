737b312ae02f0b27176405992232dab0c8418e66
jimmyzy <545303651@qq.com>
2025-11-17 13:16:14 +0000

    update AI/smart-ai.lua.
    Signed-off-by: jimmyzy <545303651@qq.com>

--- AI/smart-ai.lua
+++ AI/smart-ai.lua
@@ -155,15 +155,15 @@ end
 
 function SetInitialTables()
 	sgs.ai_type_name = {"SkillCard", "BasicCard", "TrickCard", "EquipCard"}
-	sgs.priority_skill = 	"jianan|yiji|fangzhu|tuxi|shangshi|jixi|qice|jieyue|zaoyun|" ..
-							"shouyue|paoxiao|jizhi|tieqi|liegong|jili|xuanhuo|tongdu|" ..
-							"jiahe|xiaoji|guose|tianxiang|fanjian|buqu|xuanlue|diaodu|jiediaodu|" ..
-							"hongfa|jijiu|luanji|wansha|jianchu|qianhuan|yigui|fudi|yongsi|huashen|"..
+	sgs.priority_skill = 	"jianan|yiji|fangzhu|tuxi|shangshi|jixi|qice|jieyue|zaoyun|qice|" ..
+							"shouyue|paoxiao|jizhi|tieqi|liegong|jili|xuanhuo|tongdu|zhiman|" ..
+							"jiahe|xiaoji|guose|tianxiang|fanjian|buqu|xuanlue|diaodu|jiediaodu|yinghun_sunjian|" ..
+							"hongfa|jijiu|luanji|wansha|jianchu|qianhuan|yigui|fudi|yongsi|huashen|haokui|"..
 							"paiyi|suzhi|shilu|huaiyi|chenglve|congcha|jinfa|lixia|"..
 							"zhukou|jinghe|guowu|shenwei|wanggui|boyan|kuangcai|"..
-							"miewu|guishu|sidi|danlao|wanglie|zhuidu|jiediaodu|jiejinghe"
+							"miewu|guishu|sidi|danlao|wanglie|zhuidu|jiejinghe"
 	sgs.masochism_skill = "yiji|fankui|jieming|ganglie|fangzhu|jianxiong|qianhuan|zhiyu|jihun|fudi|huashen|luashibei|" ..
-						  "bushi|shicai|quanji|zhaoxin|fankui_simazhao|wanggui|sidi|shangshi|benyu|jiehengjiang"
+						  "bushi|shicai|quanji|zhaoxin|fankui_simazhao|wanggui|sidi|shangshi|benyu|jiehengjiang|fangzhu_lordcaopi"
 	sgs.defense_skill = "qingguo|longdan|kongcheng|niepan|bazhen|kanpo|xiangle|tianxiang|liuli|qianxun|leiji|duanchang|beige|weimu|" ..
 						"tuntian|shoucheng|yicheng|qianhuan|jizhao|wanwei|enyuan|buyi|keshou|qiuan|biluan|jiancai|aocai|jieyicheng" ..
 						"xibing|zhente|qiao|shejian|yusui|deshao|yuanyu|mingzhe|jilei|shigong|dingke|shefu|taidan"
@@ -1556,7 +1556,7 @@ function SmartAI:writeKeepValue(card)
 		elseif self:needKongcheng() then return 5.0
 		end
 		local value = 0
-		if card:isKindOf("Armor") then value = self:isWeak() and 5.2 or 3.2
+		if card:isKindOf("Armor") then value = (self:isWeak() or self.player:hasSkill("luayixing")) and 5.2 or 3.2
 		elseif card:isKindOf("DefensiveHorse") then value = self:isWeak() and 4.3 or 3.19
 		elseif card:isKindOf("Weapon") then value = self.player:getPhase() == sgs.Player_Play and self:slashIsAvailable() and 3.39 or 3.2
 		elseif card:isKindOf("JadeSeal") and not self.player:hasSkill("yongsi") then value = 5
@@ -1750,7 +1750,7 @@ function SmartAI:getUseValue(card)
 		if self.player:hasSkills("qiangxi") and card:isKindOf("Weapon") then v = 2 end
 		if card:isKindOf("Crossbow") then v = v + self:getCardsNum("Slash") * 2 end
 		if self.player:hasSkills("kurou|wusheng|kuanggu|luoshen|wangxi|quanji") and card:isKindOf("Crossbow") then return 9 end
-		if self.player:hasSkills("bazhen|jgyizhong|taidan") and card:isKindOf("Armor") then v = 2 end
+		if self.player:hasSkills("bazhen|jgyizhong|taidan|luayixing") and card:isKindOf("Armor") then v = 2 end
 
 		local lvfan = sgs.findPlayerByShownSkillName("jiediaodu")
 		if lvfan and self.player:isFriendWith(lvfan) then v = 6.7 end
@@ -6247,11 +6247,6 @@ function SmartAI:getAoeValue(card)
 					value = value + ((card:isVirtualCard() and card:subcardsLength() * 10) or 10)
 				end
 
-				---------
-				if self.player:getKingdom() == "wei" and not punish then
-					value = 500
-				end
-
 			else
 				value = 0
 				if to:hasShownSkill("juxiang") and not card:isVirtualCard() then value = value + 10 end
@@ -6336,11 +6331,6 @@ function SmartAI:getAoeValue(card)
 
 				if to:getHp() > 1 and to:hasShownSkill("jianxiong") then
 					value = value + ((card:isVirtualCard() and card:subcardsLength() * 10) or 10)
-				end
-
-				----------
-				if self.player:getKingdom() == "wei" and not punish then
-					value = 500
 				end
 
 			else
@@ -6826,6 +6816,15 @@ function SmartAI:useEquipCard(card, use)
 			return
 		end
 	end
+	if card:isKindOf("jinxiuzhengpao") then
+		local lord_liushan = sgs.findPlayerByShownSkillName("luayixing") --有君刘禅在（其他势力/已有防具/自己就是君刘禅）不装备锦绣征袍
+		if lord_liushan and lord_liushan:isAlive() then
+			if not self.player:isFriendWith(lord_liushan) or (self.player:objectName() ~= lord_liushan:objectName() and 
+			self.player:getArmor()) or (self.player:objectName() == lord_liushan:objectName()) then
+				return
+			end
+		end
+	end
 	if card:isKindOf("ImperialEdict") then--宝物诏书
 		use.card = card
 		return
@@ -6872,7 +6871,7 @@ function SmartAI:useEquipCard(card, use)
 	elseif card:isKindOf("Armor") then
 		local lion = self:getCard("SilverLion")
 		if lion and self.player:isWounded() and not self.player:hasArmorEffect("SilverLion") and not card:isKindOf("SilverLion")
-			and not (self.player:hasSkills("bazhen|jgyizhong|taidan") and not self.player:getArmor()) then
+			and not (self.player:hasSkills("bazhen|jgyizhong|taidan|luayixing") and not self.player:getArmor()) then
 			use.card = lion
 			return
 		end
@@ -6881,7 +6880,7 @@ function SmartAI:useEquipCard(card, use)
 				if not friend:getArmor() then return end
 			end
 		end
-		if self.player:hasSkills("bazhen|jgyizhong|taidan") then return end
+		if self.player:hasSkills("bazhen|jgyizhong|taidan|luayixing") then return end
 		if self:evaluateArmor(card) > self:evaluateArmor() or (isenemy_zzzh and self:getOverflow() > 1) then use.card = card end
 		return
 	elseif card:isKindOf("OffensiveHorse") then
